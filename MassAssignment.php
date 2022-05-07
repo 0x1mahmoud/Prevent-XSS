32c2672fd73f60b881aacd9b0a3b753152a938a6
Mahmoud Ashraf <74935238+0x1mahmoud@users.noreply.github.com>
2022-05-07 02:30:39 +0200

    Update MassAssignment.php

--- MassAssignment.php
+++ MassAssignment.php
@@ -1,5 +1,25 @@
 <?php
 
-use App\Models\User;
+namespace App\Http\Middleware;
 
-User::where('email', $email)->get();
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        header('Access-Control-Allow-Origin:  *');
+        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        header('Access-Control-Allow-Methods:  GET, POST, PUT, PATCH, DELETE');
+
+        return $next($request);
+    }
+}
